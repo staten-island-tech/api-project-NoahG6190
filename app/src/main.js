037c29f905ce60481edfeb77f90d4a921b704496
NoahG6190 <noahg258@nycstudents.net>
2025-12-22 11:02:17 -0800

    new code 12/22/25

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+const URL = "https://fortnite-api.com/v2/cosmetics/br/name/"E";
+async function getData() {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("api-response").textContent = data.br;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+getData();
